85cb30f528849271bf741912e0510b109e6a5525
Ujjwal Kumar <19089@HINDUJA-TECH.COM>
2024-08-21 16:59:06 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ujjwalx0/', // Update this with your GitHub repository name
+  base: '/', // This is used for user/organization pages
 });
